659178c33aa4ca52f7b3d6f9b37c8ea01e2c6acf
Scott Shepherd <scottshep86@gmail.com>
2023-05-09 21:34:57 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstFive, setFirstFive] = useState(contacts.slice(5, 10));
+  const imgStyle = {
+    height: 100,
+  };
 
+  const randomContacts = () => {
+    setFirstFive((contactsFromArray) => {
+      const randomCeleb = Math.floor(Math.random() * contacts.length);
+      return [contacts[randomCeleb], ...contactsFromArray];
+    });
+  };
+
+  const sortByName = () => {
+    const sortedArrayByName = firstFive.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (b.name < a.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+      /* {a.name < b.name ? -1 : 0}
+          {b.name < a.name ? 1 : 0} */
+      });
+      
+      setFirstFive([...sortedArrayByName]);
+  };
+
+  const sortByPopularity = () => {
+    const sortedArrayByPopularity = firstFive.sort((a, b) => {
+      if (a.popularity > b.popularity) {
+        return -1;
+      }
+      if (b.popularity > a.popularity) {
+        return 1;
+      } else {
+        return 0;
+      }
+      });
+      setFirstFive([...sortedArrayByPopularity]);
+  };
+
+  const deleteContact = (currentContact) => {
+    let filteredContacts = firstFive.filter((currentElement) => {
+      return currentElement.id !== currentContact.id
+    });
+    setFirstFive(filteredContacts)
+  };
+  /* console.log(firstFive); */
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>IronContacts</h1>
+      <button onClick={randomContacts}>Add Random Contact</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {firstFive.map((element) => {
+            return (
+              <tr>
+                <th>
+                  <img
+                    style={imgStyle}
+                    src={element.pictureUrl}
+                    alt={element.name}
+                  />
+                </th>
+                <th>{element.name}</th>
+                <th>{element.popularity.toFixed(2)}</th>
+                <th>{element.wonOscar ? "🏆" : ""}</th>
+                <th>{element.wonEmmy ? "🏆" : ""}</th>
+                <button onClick={() => deleteContact(element)}>Delete</button>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
